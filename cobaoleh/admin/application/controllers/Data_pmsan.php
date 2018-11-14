0afe52546903ece1151969e50c71e018949dd4f1
ARDHAN\Ardhan <febriansyahardhan@gmail.com>
2018-11-14 08:26:00 +0700

    ini if

--- cobaoleh/admin/application/controllers/Data_pmsan.php
+++ cobaoleh/admin/application/controllers/Data_pmsan.php
@@ -2,10 +2,20 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Data_pmsan extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		$this->load->model('Datapesanan_model');
+		$this -> model = $this -> Datapesanan_model;
+		$this -> load -> database();
+		$this -> load -> helper('url');
+	}
 
 	public function index()
 	{
-		$this->load->view('Pesan/Data_pemesan');
+		$data = array(
+			'data_transaksi' => $this -> Datapesanan_model -> get_datatransaksi(),
+		);
+		$this->load->view('Pesan/Data_pemesan', $data);
 		
 	}
 
